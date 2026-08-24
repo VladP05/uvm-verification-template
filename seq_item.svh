dfbd6c332899498f8c94de772d8866a29733490a
Vlad <vladalexandrupetrila@gmail.com>
2026-08-24 14:11:04 +0300

    Added_seq_item

---
+++ seq_item.svh
@@ -0,0 +1,12 @@
+class name_seq extends uvm_sequence_item;
+    `uvm_object_utils(name_seq)
+
+    //signals
+
+    function new(string name);
+        super.new(name);
+    endfunction
+
+    //constraints
+
+endclass : name
