01607dbba40ce20c1099de78498829ad2d6b6ae8
Vlad <vladalexandrupetrila@gmail.com>
2026-08-24 14:45:14 +0300

    Added_tb_top

---
+++ tb_top.sv
@@ -0,0 +1,23 @@
+`timescale 1ns/1ps
+
+import uvm_pkg::*;
+`include "uvm_macros.svh"
+import name_pkg::*; //change!!
+
+module tb_top();
+    
+    //declaration and logic for clk and rst_n signals
+
+    //connect tb signals to interface signals
+
+    //connect interface signals to module signals
+
+    initial begin
+
+        uvm_config_db #(virtual name_if)::set(null, "*", "vif", name_if);
+
+        run_test("test"); //change!!
+
+    end
+
+endmodule : tb_top
