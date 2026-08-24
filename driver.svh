02e73881a0f045f92278587fe5dfa613de0d00ba
Vlad <vladalexandrupetrila@gmail.com>
2026-08-24 14:15:20 +0300

    Added_driver

---
+++ driver.svh
@@ -0,0 +1,31 @@
+class name_driver extends uvm_driver #(name_seq);
+    virtual SmartFridge_interface vif;
+    
+    `uvm_component_utils(name_driver)
+
+    function new(string name, uvm_component parent);
+        super.new(name, parent);
+    endfunction
+
+    function void build_phase(uvm_phase phase);
+        super.build_phase(phase);
+        if(!uvm_config_db#(virtual SmartFridge_interface)::get(this, "", "vif", vif)) begin
+            `uvm_fatal("NO_VIF", {"Virtual interface must be set for:", get_full_name(), ".vif"});
+        end
+        
+    endfunction : build_phase
+
+    virtual task run_phase(uvm_phase phase);
+        
+       //initialize all input interface signals with 0
+        
+        forever begin
+            seq_item_port.get_next_item(req);
+            
+            //driver logic
+
+            seq_item_port.item_done();
+        end
+    endtask : run_phase
+
+endclass : name_driver
